9145268f8325241a629d2d3e44f6b28341671b6f
Craig Fleming <rythos42@gmail.com>
2017-09-26 16:19:18 -0700

    Added Ghar Rebel and Empire Added debug of just outputing the base text Added ability to move rows around

--- ParseAntaresPdf/CopyData.bat
+++ ParseAntaresPdf/CopyData.bat
@@ -1,25 +1,26 @@
 @echo off
 
-REM For each data file, copy it to a non-temporary form, copy it to the web site and delete the temporary file.
+REM For each data file, copy it to the web site and delete the temporary file.
 REM Only do this after you are sure the temporary files are all correct.
 
-copy .\bin\Debug\Algoryn-new.xml .\bin\Debug\Algoryn.xml
-copy .\bin\Debug\Algoryn-new.xml ..\Web\armylists\Algoryn.xml
+copy .\bin\Debug\Algoryn-new.xml ..\Web\armylists\Algoryn.xml > nul
 del .\bin\Debug\Algoryn-new.xml
 
-copy .\bin\Debug\Boromite-new.xml .\bin\Debug\Boromite.xml
-copy .\bin\Debug\Boromite-new.xml ..\Web\armylists\Boromite.xml
+copy .\bin\Debug\Boromite-new.xml ..\Web\armylists\Boromite.xml  > nul
 del .\bin\Debug\Boromite-new.xml
 
-copy .\bin\Debug\Concord-new.xml .\bin\Debug\Concord.xml
-copy .\bin\Debug\Concord-new.xml ..\Web\armylists\Concord.xml
+copy .\bin\Debug\Concord-new.xml ..\Web\armylists\Concord.xml > nul
 del .\bin\Debug\Concord-new.xml
 
-copy .\bin\Debug\Freeborn-new.xml .\bin\Debug\Freeborn.xml
-copy .\bin\Debug\Freeborn-new.xml ..\Web\armylists\Freeborn.xml
+copy .\bin\Debug\Freeborn-new.xml ..\Web\armylists\Freeborn.xml > nul
 del .\bin\Debug\Freeborn-new.xml
 
-copy .\bin\Debug\Isorian-new.xml .\bin\Debug\Isorian.xml
-copy .\bin\Debug\Isorian-new.xml ..\Web\armylists\Isorian.xml
+copy .\bin\Debug\Isorian-new.xml ..\Web\armylists\Isorian.xml > nul
 del .\bin\Debug\Isorian-new.xml
+
+copy ".\bin\Debug\Ghar Empire-new.xml" "..\Web\armylists\Ghar Empire.xml" > nul
+del ".\bin\Debug\Ghar Empire-new.xml"
+
+copy ".\bin\Debug\Ghar Rebel-new.xml" "..\Web\armylists\Ghar Rebel.xml" > nul
+del ".\bin\Debug\Ghar Rebel-new.xml"
 
--- ParseAntaresPdf/GenerateAll.bat
+++ ParseAntaresPdf/GenerateAll.bat
@@ -6,4 +6,22 @@ REM Generate all data files to temporary files, so we can do a diff on the exist
 .\bin\Debug\ParseAntaresPdf.exe -pdf ".\bin\Debug\Boromite Army List Antares V2.0 pdf.pdf" -out .\bin\Debug\Boromite-new.xml
 .\bin\Debug\ParseAntaresPdf.exe -pdf ".\bin\Debug\Concord Antares Army List V2.0 pdf.pdf" -out .\bin\Debug\Concord-new.xml
 .\bin\Debug\ParseAntaresPdf.exe -pdf ".\bin\Debug\Freeborn Antares Army List V2.0 pdf.pdf" -out .\bin\Debug\Freeborn-new.xml
-.\bin\Debug\ParseAntaresPdf.exe -pdf ".\bin\Debug\Isorian Army List Antares V2.0 pdf.pdf" -out .\bin\Debug\Isorian-new.xml
+.\bin\Debug\ParseAntaresPdf.exe -pdf ".\bin\Debug\Isorian Army List Antares V2.1 pdf.pdf" -out .\bin\Debug\Isorian-new.xml
+.\bin\Debug\ParseAntaresPdf.exe -pdf ".\bin\Debug\Ghar Empire Army List V2.0 pdf.pdf" -out ".\bin\Debug\Ghar Empire-new.xml"
+.\bin\Debug\ParseAntaresPdf.exe -pdf ".\bin\Debug\Ghar Rebel Army List Antares V2.0 pdf.pdf" -out ".\bin\Debug\Ghar Rebel-new.xml"
+
+fc ..\Web\armylists\Algoryn.xml .\bin\Debug\Algoryn-new.xml
+IF ERRORLEVEL 1 ECHO Algoryn-new.xml not the same as original. Check differences are correct before using CopyData.bat.
+fc ..\Web\armylists\Boromite.xml .\bin\Debug\Boromite-new.xml 
+IF ERRORLEVEL 1 ECHO Boromite-new.xml not the same as original. Check differences are correct before using CopyData.bat.
+fc ..\Web\armylists\Concord.xml .\bin\Debug\Concord-new.xml
+IF ERRORLEVEL 1 ECHO Concord-new.xml not the same as original. Check differences are correct before using CopyData.bat.
+fc ..\Web\armylists\Freeborn.xml .\bin\Debug\Freeborn-new.xml
+IF ERRORLEVEL 1 ECHO Freeborn-new.xml not the same as original. Check differences are correct before using CopyData.bat.
+fc ..\Web\armylists\Isorian.xml .\bin\Debug\Isorian-new.xml
+IF ERRORLEVEL 1 ECHO Isorian-new.xml not the same as original. Check differences are correct before using CopyData.bat.
+fc "..\Web\armylists\Ghar Empire.xml" ".\bin\Debug\Ghar Empire-new.xml"
+IF ERRORLEVEL 1 ECHO Ghar Empire-new.xml not the same as original. Check differences are correct before using CopyData.bat.
+fc "..\Web\armylists\Ghar Rebel.xml" ".\bin\Debug\Ghar Rebel-new.xml"
+IF ERRORLEVEL 1 ECHO Ghar Rebel-new.xml not the same as original. Check differences are correct before using CopyData.bat.
+
